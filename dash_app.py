39dae436f40c03da92ae5497c0aa3f92aed0e179
KamenDamov <kamendamov123@gmail.com>
2022-07-12 00:05:58 -0400

    push

--- dash_app.py
+++ dash_app.py
@@ -1,4 +1,5 @@
 #Imports
+from click import style
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -8,41 +9,78 @@ import plotly.express as px
 import functools as f
 import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, ALL, State
 
-df = pd.read_csv('Meteorite_Landings.csv')
-df = df.dropna()
+df = pd.read_csv('meteorites_new')
+print(df.info())
+df = df.drop('Unnamed: 0', axis=1)
+
+fig = go.Figure(
+    data=go.Scattergeo(
+        lon = df['reclong'],
+        lat = df['reclat'],
+        text = df['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            opacity = 0.8,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'circle',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = 'Viridis',
+            cmin = 0,
+            color = df['mass (g)'],
+            cmax = df['mass (g)'].max(),
+            colorbar_title="Mass of meteorite (in grams)"
+        )
+    )
+)
+
+fig.update_layout(
+        title = 'Meteorite landings',
+        geo = dict(
+            showland = True,
+            landcolor = "rgb(100, 100, 100)",
+            subunitcolor = "rgb(0, 0, 0)",
+            countrycolor = "rgb(0, 0, 0)",
+            countrywidth = 1,
+            subunitwidth = 1
+        )
+    )
 
 app = dash.Dash(__name__)
 app.layout = html.Div(
     children=[html.Div(
             children=[
                 html.H1(
-                    children="meteorites",style={'textAlign': 'center'}, className="header-title" 
+                    children="Meteorites fallen on earth",
+                    style={'textAlign': 'center'}, 
+                    className="header-title" 
                 ), #Header title
                 html.H2(
-                    children="Meteorites fallen on earth",
-                    className="header-description", style={'textAlign': 'center'},
+                    children="A dashboard visualize one of humanity's biggest existential threats",
+                    className="header-description", 
+                    style={'textAlign': 'center'},
                 ),
             ],
-            className="header",style={'backgroundColor':'#F5F5F5'},
-    ),#Description below the header
-        
-        
+            className="header",
+    ),#Description below the header        
         html.Div(
             children=[
                 html.Div(children = 'Year', style={'fontSize': "24px"},className = 'menu-title'),
                 dcc.Dropdown(
                     id = 'year-filter',
                     options = [
-                        {'label': Year, 'value':Year}
-                       for Year in np.sort(df.year.unique())
+                        {'label': Year, 'value': Year}
+                       for Year in np.append(np.sort(df.year.unique()),"All")
                     ], #'Year' is the filter
-                    value ='2010',
-                    clearable = False,
-                    searchable = False,
+                    value = "All",
                     className = 'dropdown', style={'fontSize': "24px",'textAlign': 'center'},
                 ),
             ],
@@ -50,21 +88,81 @@ app.layout = html.Div(
 ),
 html.Div(
             children=[
-                html.Div(children = 'Fall', style={'fontSize': "24px"},className = 'menu-title'),
-                dcc.Dropdown(
-                    id = 'fell-found-filter',
-                    options = [
-                        {'label': fall, 'value':fall}
-                       for fall in np.sort(df.fall.unique())
-                    ], #'Year' is the filter
-                    value ='2010',
-                    clearable = False,
-                    searchable = False,
-                    className = 'dropdown', style={'fontSize': "24px",'textAlign': 'center'},
+                html.Div(children = 'Want to see the most significant meteorites in history', style={'fontSize': "24px"},className = 'menu-title'),
+                dcc.Checklist(
+                    ['Yes'],
+                    id = "checklist",
+                    inline=True,
+                    value = ""
+                    ),
+                ],
+            className = 'menu'),
+
+#Adding the world visual
+html.Div(
+            children=[
+                html.Div(
+                children = dcc.Graph(
+                    id = 'world_chart',
+                    figure = fig,
                 ),
-            ],
-            className = 'menu')
+                style={'width': '100%', 'display': 'inline-block'},
+            )])
 ]) #the dropdown function
+
+#Callback to create interaction
+@app.callback(
+    dash.dependencies.Output("world_chart", "figure"), 
+    [dash.dependencies.Input("year-filter", "value"),
+    dash.dependencies.Input("checklist", "value")],
+)
+
+def update_charts(Year, val):
+    if val == ["Yes"] and Year == "All": 
+        filtered_data = df[df['top 1% mass']==True]
+    else:
+        if Year == "All": 
+            filtered_data = df
+        else:
+            Year = int(Year)
+            filtered_data = df[df["year"] == Year]
+    
+    fig = go.Figure(data=go.Scattergeo(
+        lon = filtered_data['reclong'],
+        lat = filtered_data['reclat'],
+        text = filtered_data['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            opacity = 0.8,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'circle',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = 'Viridis',
+            cmin = 0,
+            color = filtered_data['mass (g)'],
+            cmax = filtered_data['mass (g)'].max(),
+            colorbar_title="Mass of meteorite"
+        )
+    )
+)
+    return fig
+
+fig.update_layout(
+        title = 'Meteorite landings',
+        geo = dict(
+            showland = True,
+            landcolor = "rgb(100, 100, 100)",
+            subunitcolor = "rgb(0, 0, 0)",
+            countrycolor = "rgb(0, 0, 0)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5
+        ),
+    )
 
 #Running dashboard
 if __name__ == '__main__':
